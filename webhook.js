e3ed19daca3be6a5158fe0aeacf38f4b5d558a93
Camryn Terlouw <cft07904@gmail.com>
2025-04-29 23:00:10 +0200

    feat(webhook): test push

--- webhook.js
+++ webhook.js
@@ -18,12 +18,14 @@ app.use(express.json({
 app.use(bodyParser.json());
 
 function verifySignature(req) {
-  const sig = req.headers['x-hub-signature-256'];
-  const hmac = crypto.createHmac('sha256', SECRET);
-  const digest = 'sha256=' + hmac.update(req.rawBody).digest('hex');
-  return crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(digest));
-}
-
+    const sig = req.headers['x-hub-signature-256'];
+    if (!sig) return false; 
+  
+    const hmac = crypto.createHmac('sha256', SECRET);
+    const digest = 'sha256=' + hmac.update(req.rawBody).digest('hex');
+    return crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(digest));
+  }
+  
 app.post('/github-webhook', (req, res) => {
   if (!verifySignature(req)) {
     return res.status(401).send('Invalid signature');
@@ -36,7 +38,7 @@ app.post('/github-webhook', (req, res) => {
     }
     console.log('Git pull output:', stdout);
 
-    exec(`cd ${CLIENT_PATH} && npm install && npm run build`, (err, stdout, stderr) => {
+    exec(`cd ${CLIENT_PATH} && npm i --force && npm run build`, (err, stdout, stderr) => {
       if (err) {
         console.error('Next.js build error:', stderr);
         return res.status(500).send('Client build failed');
