48bc0552f0578aa093adbad64bc6a0461bbbc6d1
Camryn Terlouw <cft07904@gmail.com>
2025-04-29 15:34:12 +0200

    feat(index): added some test components on the index page

--- client/src/components/ui/apple-cards-carousel.tsx
+++ client/src/components/ui/apple-cards-carousel.tsx
@@ -11,12 +11,10 @@ import React, {
 import {
   IconArrowNarrowLeft,
   IconArrowNarrowRight,
-  IconX,
 } from "@tabler/icons-react";
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "motion/react";
-import Image, { ImageProps } from "next/image";
-import { useOutsideClick } from "@/hooks/use-outside-click";
+import { ImageProps } from "next/image";
 
 interface CarouselProps {
   items: JSX.Element[];
